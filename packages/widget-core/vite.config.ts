7bc679eaa9a0d2ce73e92611831cbe3d8952b510
Eugen Istoc <eugenistoc@gmail.com>
2024-09-25 20:32:39 -0400

    refactor: build configuration to use absolute path for entry file

--- packages/widget-core/vite.config.ts
+++ packages/widget-core/vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
+import { resolve } from 'path'
 
 export default defineConfig({
   build: {
     lib: {
-      entry: 'src/index.ts',
+      entry: resolve(__dirname, 'src/index.ts'),
       formats: ['es'],
       fileName: 'index',
     },
